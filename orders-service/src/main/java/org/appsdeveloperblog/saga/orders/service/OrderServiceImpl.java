d79dfa0f19be1e644c1c04a4f4b8f83da58e1a39
gots <sergei.gots@yahoo.com>
2026-03-08 12:41:51 +0300

    feat: send OrderCreatedEvent to Kafka in OrderService

--- orders-service/src/main/java/org/appsdeveloperblog/saga/orders/service/OrderServiceImpl.java
+++ orders-service/src/main/java/org/appsdeveloperblog/saga/orders/service/OrderServiceImpl.java
@@ -1,17 +1,28 @@
 package org.appsdeveloperblog.saga.orders.service;
 
 import org.appsdeveloperblog.saga.core.dto.Order;
+import org.appsdeveloperblog.saga.core.dto.events.OrderCreatedEvent;
 import org.appsdeveloperblog.saga.core.types.OrderStatus;
 import org.appsdeveloperblog.saga.orders.dao.jpa.entity.OrderEntity;
 import org.appsdeveloperblog.saga.orders.dao.jpa.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-    private final OrderRepository orderRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    private final OrderRepository orderRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String orderEventsTopicName;
+
+    public OrderServiceImpl(
+            OrderRepository orderRepository,
+            KafkaTemplate<String, Object> kafkaTemplate,
+            @Value("${order.events.topic.name}") String orderEventsTopicName) {
         this.orderRepository = orderRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.orderEventsTopicName = orderEventsTopicName;
     }
 
     @Override
@@ -21,7 +32,15 @@ public class OrderServiceImpl implements OrderService {
         entity.setProductId(order.getProductId());
         entity.setProductQuantity(order.getProductQuantity());
         entity.setStatus(OrderStatus.CREATED);
-        orderRepository.save(entity);
+        OrderEntity savedEntity = orderRepository.save(entity);
+
+        OrderCreatedEvent placedOrderEvent = new OrderCreatedEvent();
+        placedOrderEvent.setOrderId(savedEntity.getId());
+        placedOrderEvent.setProductId(savedEntity.getProductId());
+        placedOrderEvent.setCustomerId(savedEntity.getCustomerId());
+        placedOrderEvent.setProductQuantity(placedOrderEvent.getProductQuantity());
+
+        kafkaTemplate.send(orderEventsTopicName, placedOrderEvent);
 
         return new Order(
                 entity.getId(),
