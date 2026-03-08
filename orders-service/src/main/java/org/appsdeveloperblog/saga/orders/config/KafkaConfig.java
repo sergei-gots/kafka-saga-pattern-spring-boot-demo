2062789ecd2536c57c395df12938b86ca13f640a
gots <sergei.gots@yahoo.com>
2026-03-08 12:39:22 +0300

    feat(orders-service): add automatic creation of order-events Kafka topic

--- orders-service/src/main/java/org/appsdeveloperblog/saga/orders/config/KafkaConfig.java
+++ orders-service/src/main/java/org/appsdeveloperblog/saga/orders/config/KafkaConfig.java
@@ -1,16 +1,34 @@
 package org.appsdeveloperblog.saga.orders.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
 
+    private final int TOPIC_PARTITIONS = 3;
+    private final int TOPIC_REPLICAS = 3;
+
+    @Value("${order.events.topic.name}")
+    private String orderEventsTopicName;
+
     @Bean
     KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    NewTopic createOrderEventsTopic() {
+        return TopicBuilder
+                .name(orderEventsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICAS)
+        .build();
     }
 
 }
